f9bd7b8ff13942e194d7ca3e22bdebd2e88041cd
Pooja <poojakallada1518@gmail.com>
2025-06-09 23:16:14 +0530

    added answer

--- FSHospitalWebsite/chatbot/chatbot.py
+++ FSHospitalWebsite/chatbot/chatbot.py
@@ -1,268 +1,157 @@
-import re
+from flask import Flask, request, jsonify, send_from_directory
+from flask_cors import CORS
 import pandas as pd
-import pyttsx3
 from sklearn import preprocessing
-from sklearn.tree import DecisionTreeClassifier,_tree
-import numpy as np
+from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
-from sklearn.model_selection import cross_val_score
-from sklearn.svm import SVC
+import numpy as np
 import csv
 import warnings
+import os
+
 warnings.filterwarnings("ignore", category=DeprecationWarning)
 
+# Set the path to the frontend folder, which is one level up
+app = Flask(__name__, static_folder='../frontend', template_folder='../frontend')
+CORS(app)  # Enable CORS to allow cross-origin requests
 
-training = pd.read_csv('Data/Training.csv')
-testing= pd.read_csv('Data/Testing.csv')
-cols= training.columns
-cols= cols[:-1]
+# Load training and testing data
+training = pd.read_csv('data/Training.csv')
+cols = training.columns[:-1]  # symptom columns
 x = training[cols]
 y = training['prognosis']
-y1= y
 
-
-reduced_data = training.groupby(training['prognosis']).max()
-
-#mapping strings to numbers
+# Encode target labels
 le = preprocessing.LabelEncoder()
 le.fit(y)
-y = le.transform(y)
+y_encoded = le.transform(y)
 
+# Train Decision Tree model
+x_train, x_test, y_train, y_test = train_test_split(x, y_encoded, test_size=0.33, random_state=42)
+clf = DecisionTreeClassifier().fit(x_train, y_train)
 
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.33, random_state=42)
-testx    = testing[cols]
-testy    = testing['prognosis']  
-testy    = le.transform(testy)
-
-
-clf1  = DecisionTreeClassifier()
-clf = clf1.fit(x_train,y_train)
-# print(clf.score(x_train,y_train))
-# print ("cross result========")
-scores = cross_val_score(clf, x_test, y_test, cv=3)
-# print (scores)
-print (scores.mean())
-
-
-model=SVC()
-model.fit(x_train,y_train)
-print("for svm: ")
-print(model.score(x_test,y_test))
-
-importances = clf.feature_importances_
-indices = np.argsort(importances)[::-1]
-features = cols
-
-def readn(nstr):
-    engine = pyttsx3.init()
-
-    engine.setProperty('voice', "english+f5")
-    engine.setProperty('rate', 130)
-
-    engine.say(nstr)
-    engine.runAndWait()
-    engine.stop()
-
-
-severityDictionary=dict()
-description_list = dict()
-precautionDictionary=dict()
-
-symptoms_dict = {}
-
-for index, symptom in enumerate(x):
-       symptoms_dict[symptom] = index
-def calc_condition(exp,days):
-    sum=0
-    for item in exp:
-         sum=sum+severityDictionary[item]
-    if((sum*days)/(len(exp)+1)>13):
-        print("You should take the consultation from doctor. ")
-    else:
-        print("It might not be that bad but you should take precautions.")
-
+# Load symptom description, severity, and precaution dictionaries
+description_list = {}
+severityDictionary = {}
+precautionDictionary = {}
 
 def getDescription():
-    global description_list
-    with open('MasterData/symptom_Description.csv') as csv_file:
+    with open('masterdata/symptom_Description.csv') as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
-        line_count = 0
         for row in csv_reader:
-            _description={row[0]:row[1]}
-            description_list.update(_description)
-
-
-
+            description_list[row[0].lower()] = row[1]
 
 def getSeverityDict():
-    global severityDictionary
-    with open('MasterData/symptom_severity.csv') as csv_file:
-
+    with open('masterdata/symptom_severity.csv') as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
-        line_count = 0
-        try:
-            for row in csv_reader:
-                _diction={row[0]:int(row[1])}
-                severityDictionary.update(_diction)
-        except:
-            pass
-
+        for row in csv_reader:
+            try:
+                severityDictionary[row[0].lower()] = int(row[1])
+            except:
+                pass
 
 def getprecautionDict():
-    global precautionDictionary
-    with open('MasterData/symptom_precaution.csv') as csv_file:
-
+    with open('masterdata/symptom_precaution.csv') as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
-        line_count = 0
         for row in csv_reader:
-            _prec={row[0]:[row[1],row[2],row[3],row[4]]}
-            precautionDictionary.update(_prec)
+            precautionDictionary[row[0].lower()] = [row[1], row[2], row[3], row[4]]
 
-
-def getInfo():
-    print("-----------------------------------HealthCare ChatBot-----------------------------------")
-    print("\nYour Name? \t\t\t\t",end="->")
-    name=input("")
-    print("Hello, ",name)
-
-def check_pattern(dis_list,inp):
-    pred_list=[]
-    inp=inp.replace(' ','_')
-    patt = f"{inp}"
-    regexp = re.compile(patt)
-    pred_list=[item for item in dis_list if regexp.search(item)]
-    if(len(pred_list)>0):
-        return 1,pred_list
-    else:
-        return 0,[]
-def sec_predict(symptoms_exp):
-    df = pd.read_csv('Data/Training.csv')
-    X = df.iloc[:, :-1]
-    y = df['prognosis']
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=20)
-    rf_clf = DecisionTreeClassifier()
-    rf_clf.fit(X_train, y_train)
-
-    symptoms_dict = {symptom: index for index, symptom in enumerate(X)}
-    input_vector = np.zeros(len(symptoms_dict))
-    for item in symptoms_exp:
-      input_vector[[symptoms_dict[item]]] = 1
-
-    return rf_clf.predict([input_vector])
-
-
-def print_disease(node):
-    node = node[0]
-    val  = node.nonzero() 
-    disease = le.inverse_transform(val[0])
-    return list(map(lambda x:x.strip(),list(disease)))
-
-def tree_to_code(tree, feature_names):
-    tree_ = tree.tree_
-    feature_name = [
-        feature_names[i] if i != _tree.TREE_UNDEFINED else "undefined!"
-        for i in tree_.feature
-    ]
-
-    chk_dis=",".join(feature_names).split(",")
-    symptoms_present = []
-
-    while True:
-
-        print("\nEnter the symptom you are experiencing  \t\t",end="->")
-        disease_input = input("")
-        conf,cnf_dis=check_pattern(chk_dis,disease_input)
-        if conf==1:
-            print("searches related to input: ")
-            for num,it in enumerate(cnf_dis):
-                print(num,")",it)
-            if num!=0:
-                print(f"Select the one you meant (0 - {num}):  ", end="")
-                conf_inp = int(input(""))
-            else:
-                conf_inp=0
-
-            disease_input=cnf_dis[conf_inp]
-            break
-            # print("Did you mean: ",cnf_dis,"?(yes/no) :",end="")
-            # conf_inp = input("")
-            # if(conf_inp=="yes"):
-            #     break
-        else:
-            print("Enter valid symptom.")
-
-    while True:
-        try:
-            num_days=int(input("Okay. From how many days ? : "))
-            break
-        except:
-            print("Enter valid input.")
-    def recurse(node, depth):
-        indent = "  " * depth
-        if tree_.feature[node] != _tree.TREE_UNDEFINED:
-            name = feature_name[node]
-            threshold = tree_.threshold[node]
-
-            if name == disease_input:
-                val = 1
-            else:
-                val = 0
-            if  val <= threshold:
-                recurse(tree_.children_left[node], depth + 1)
-            else:
-                symptoms_present.append(name)
-                recurse(tree_.children_right[node], depth + 1)
-        else:
-            present_disease = print_disease(tree_.value[node])
-            # print( "You may have " +  present_disease )
-            red_cols = reduced_data.columns 
-            symptoms_given = red_cols[reduced_data.loc[present_disease].values[0].nonzero()]
-            # dis_list=list(symptoms_present)
-            # if len(dis_list)!=0:
-            #     print("symptoms present  " + str(list(symptoms_present)))
-            # print("symptoms given "  +  str(list(symptoms_given)) )
-            print("Are you experiencing any ")
-            symptoms_exp=[]
-            for syms in list(symptoms_given):
-                inp=""
-                print(syms,"? : ",end='')
-                while True:
-                    inp=input("")
-                    if(inp=="yes" or inp=="no"):
-                        break
-                    else:
-                        print("provide proper answers i.e. (yes/no) : ",end="")
-                if(inp=="yes"):
-                    symptoms_exp.append(syms)
-
-            second_prediction=sec_predict(symptoms_exp)
-            # print(second_prediction)
-            calc_condition(symptoms_exp,num_days)
-            if(present_disease[0]==second_prediction[0]):
-                print("You may have ", present_disease[0])
-                print(description_list[present_disease[0]])
-
-                # readn(f"You may have {present_disease[0]}")
-                # readn(f"{description_list[present_disease[0]]}")
-
-            else:
-                print("You may have ", present_disease[0], "or ", second_prediction[0])
-                print(description_list[present_disease[0]])
-                print(description_list[second_prediction[0]])
-
-            # print(description_list[present_disease[0]])
-            precution_list=precautionDictionary[present_disease[0]]
-            print("Take following measures : ")
-            for  i,j in enumerate(precution_list):
-                print(i+1,")",j)
-
-            # confidence_level = (1.0*len(symptoms_present))/len(symptoms_given)
-            # print("confidence level is " + str(confidence_level))
-
-    recurse(0, 1)
+# Initialize dictionaries
 getSeverityDict()
 getDescription()
 getprecautionDict()
-getInfo()
-tree_to_code(clf,cols)
-print("----------------------------------------------------------------------------------------")
+
+# Lowercase symptom columns for matching
+cols_lower = [c.lower() for c in cols]
+
+@app.route('/chat', methods=['POST'])
+def chat():
+    data = request.get_json()
+    user_message = data.get('message', '')
+
+    # Process multiple symptoms separated by commas
+    symptoms = [s.strip().lower().replace(' ', '_') for s in user_message.split(',')]
+    
+    input_vector = np.zeros(len(cols))
+    unknown_symptoms = []
+
+    for symptom in symptoms:
+        if symptom in cols_lower:
+            idx = cols_lower.index(symptom)
+            input_vector[idx] = 1
+        else:
+            unknown_symptoms.append(symptom)
+
+    if unknown_symptoms:
+        return jsonify({'reply': f"Sorry, I didn't recognize these symptoms: {', '.join(unknown_symptoms)}. Please check the spelling and try again."})
+
+    # Predict disease
+    prediction = clf.predict([input_vector])
+    disease = le.inverse_transform(prediction)[0]
+
+    # Prepare response
+    desc = description_list.get(disease.lower(), "No description available.")
+    precautions = precautionDictionary.get(disease.lower(), [])
+
+    precautions_text = ', '.join([p for p in precautions if p]) if precautions else "No specific precautions available."
+
+    response = (
+        f"You may have: {disease}\n\n"
+        f"Description: {desc}\n\n"
+        f"Precautions: {precautions_text}"
+    )
+
+    return jsonify({'reply': response})
+
+# Serve the HTML files
+@app.route('/')
+def serve_html():
+    return send_from_directory('../frontend', 'chatbot.html')
+
+@app.route('/index')
+def serve_index():
+    return send_from_directory('../frontend', 'index.html')
+
+@app.route('/about')
+def serve_about():
+    return send_from_directory('../frontend', 'about.html')
+
+@app.route('/contact')
+def serve_contact():
+    return send_from_directory('../frontend', 'contact.html')
+
+@app.route('/medreport')
+def serve_medreport():
+    return send_from_directory('../frontend', 'medreport.html')
+
+@app.route('/notification')
+def serve_notification():
+    return send_from_directory('../frontend', 'notification.html')
+
+@app.route('/patientdashboard')
+def serve_patientdashboard():
+    return send_from_directory('../frontend', 'patientdashboard.html')
+
+@app.route('/prescription')
+def serve_prescription():
+    return send_from_directory('../frontend', 'prescription.html')
+
+@app.route('/profile')
+def serve_profile():
+    return send_from_directory('../frontend', 'profile.html')
+
+@app.route('/register')
+def serve_register():
+    return send_from_directory('../frontend', 'register.html')
+
+@app.route('/total-login')
+def serve_total_login():
+    return send_from_directory('../frontend', 'total-login.html')
+
+# Serve static files (CSS, images, etc.)
+@app.route('/<path:path>')
+def serve_static(path):
+    return send_from_directory('../frontend', path)
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
